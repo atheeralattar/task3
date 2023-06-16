280f45aba3baea4fa89cf62c051ba05af67dbe21
Atheer Alattar <aalattar@tibco.com>
2023-06-16 10:49:25 -0500

    pushing task3

---
+++ task3_solution/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+
+/*
+ * 1. Write a program in C to create and display a doubly linked list.
+Test Data :
+Input the number of nodes : 3
+Input data for node 1 : 2
+Input data for node 2 : 5
+Input data for node 3 : 8
+Expected Output :
+Data entered on the list are :                                                                               
+ node 1 : 2                                                                                                   
+ node 2 : 5                                                                                                   
+ node 3 : 8 
+
+ 
+2. Write a program in C to create a doubly linked list and display it in reverse order.
+Test Data :
+Input the number of nodes : 3
+Input data for node 1 : 2
+Input data for node 2 : 5
+Input data for node 3 : 8
+Expected Output :
+Data in reverse order are :                                                                                  
+ Data in node 1 : 8                                                                                           
+ Data in node 2 : 5                                                                                           
+ Data in node 3 : 2 
+
+ 
+3. Write a program in C to insert a node at the beginning of a doubly linked list.
+Test Data and Expected Output :
+Input the number of nodes : 3                                                                                
+ Input data for node 1 : 2                                                                                    
+ Input data for node 2 : 5                                                                                    
+ Input data for node 3 : 8                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 2                                                                                                   
+ node 2 : 5                                                                                                   
+ node 3 : 8                                                                                                   
+ Input data for the first node : 1                                                                            
+                                                                                                              
+ After insertion the new list are :                                                                           
+ node 1 : 1                                                                                                   
+ node 2 : 2                                                                                                   
+ node 3 : 5                                                                                                   
+ node 4 : 8  
+
+ 
+4. Write a program in C to insert a new node at the end of a doubly linked list.
+Test Data and Expected Output :
+Input the number of nodes : 3                                                                                
+ Input data for node 1 : 2                                                                                    
+ Input data for node 2 : 5                                                                                    
+ Input data for node 3 : 8                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 2                                                                                                   
+ node 2 : 5                                                                                                   
+ node 3 : 8                                                                                                   
+ Input data for the last node : 9                                                                             
+                                                                                                              
+ After insertion the new list are :                                                                           
+ node 1 : 2                                                                                                   
+ node 2 : 5                                                                                                   
+ node 3 : 8                                                                                                   
+ node 4 : 9 
+
+ 
+5. Write a program in C to insert a new node at any position in a doubly linked list.
+Test Data and Expected Output :
+Input the number of nodes (3 or more ): 3                                                                    
+ Input data for node 1 : 2                                                                                    
+ Input data for node 2 : 4                                                                                    
+ Input data for node 3 : 5                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 2                                                                                                   
+ node 2 : 4                                                                                                   
+ node 3 : 5                                                                                                   
+ Input the position ( 2 to 2 ) to insert a new node : 2                                                       
+ Input data for the position 2 : 3                                                                            
+                                                                                                              
+ After insertion the new list are :                                                                           
+ node 1 : 2                                                                                                   
+ node 2 : 3                                                                                                   
+ node 3 : 4                                                                                                   
+ node 4 : 5 
+
+ 
+6. Write a program in C to insert a new node in the middle of a doubly linked list.
+Test Data and Expected Output :
+ Doubly Linked List : Insert new node at the middle in a doubly linked list :                                 
+----------------------------------------------------------------------------------                            
+ Input the number of nodes (3 or more ): 3                                                                    
+ Input data for node 1 : 2                                                                                    
+ Input data for node 2 : 4                                                                                    
+ Input data for node 3 : 5                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 2                                                                                                   
+ node 2 : 4                                                                                                   
+ node 3 : 5                                                                                                   
+ Input the position ( 2 to 2 ) to insert a new node : 2                                                       
+ Input data for the position 2 : 3                                                                            
+                                                                                                              
+ After insertion the new list are :                                                                           
+ node 1 : 2                                                                                                   
+ node 2 : 3                                                                                                   
+ node 3 : 4                                                                                                   
+ node 4 : 5
+
+ 
+7. Write a program in C to delete a node from the beginning of a doubly linked list.
+Test Data and Expected Output :
+Input the number of nodes (3 or more ): 3                                                                    
+ Input data for node 1 : 1                                                                                    
+ Input data for node 2 : 2                                                                                    
+ Input data for node 3 : 3                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 1                                                                                                   
+ node 2 : 2                                                                                                   
+ node 3 : 3                                                                                                   
+                                                                                                              
+ After deletion the new list are :                                                                            
+ node 1 : 2                                                                                                   
+ node 2 : 3   
+
+ 
+8. Write a program in C to delete a node from the last node of a doubly linked list.
+Test Data and Expected Output :
+Input the number of nodes (3 or more ): 3                                                                    
+ Input data for node 1 : 1                                                                                    
+ Input data for node 2 : 2                                                                                    
+ Input data for node 3 : 3                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 1                                                                                                   
+ node 2 : 2                                                                                                   
+ node 3 : 3                                                                                                   
+                                                                                                              
+ After deletion the new list are :                                                                            
+ node 1 : 1                                                                                                   
+ node 2 : 2    
+
+ 
+9. Write a program in C to delete a node from any position in a doubly linked list.
+Test Data and Expected Output :
+Doubly Linked List : Delete node from any position of a doubly linked list :                                 
+----------------------------------------------------------------------------------                            
+ Input the number of nodes (3 or more ): 3                                                                    
+ Input data for node 1 : 1                                                                                    
+ Input data for node 2 : 2                                                                                    
+ Input data for node 3 : 3                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 1                                                                                                   
+ node 2 : 2                                                                                                   
+ node 3 : 3                                                                                                   
+ Input the position ( 1 to 3 ) to delete a node : 3                                                           
+                                                                                                              
+ After deletion the new list are :                                                                            
+ node 1 : 1                                                                                                   
+ node 2 : 2 
+
+ 
+10. Write a program in C to delete a node from the middle of a doubly linked list.
+Test Data and Expected Output :
+Input the number of nodes (3 or more ): 3                                                                    
+ Input data for node 1 : 1                                                                                    
+ Input data for node 2 : 2                                                                                    
+ Input data for node 3 : 3                                                                                    
+                                                                                                              
+ Data entered in the list are :                                                                               
+ node 1 : 1                                                                                                   
+ node 2 : 2                                                                                                   
+ node 3 : 3                                                                                                   
+ Input the position ( 1 to 3 ) to delete a node : 2                                                           
+                                                                                                              
+ After deletion the new list are :                                                                            
+ node 1 : 1                                                                                                   
+ node 2 : 3   
+
+ 
+11. Write a program in C to find the maximum value in a doubly linked list.
+Test Data :
+Input the number of nodes : 3
+Input data for node 1 : 5
+Input data for node 2 : 9
+Input data for node 3 : 1
+Expected Output :
+Data entered in the list are :                                                                               
+ node 1 : 5                                                                                                   
+ node 2 : 9                                                                                                   
+ node 3 : 1                                                                                                   
+                                                                                                              
+ The Maximum Value in the Linked List : 9  
+
+
+
+ 
+ 
+12. Write a C program to convert a Doubly Linked list into a string.
+Test Data and Expected Output :
+Input the number of nodes: 4
+ Input data for node 1 : 10
+ Input data for node 2 : 11
+ Input data for node 3 : 12
+ Input data for node 4 : 13
+
+The doubly linked list in string format: 10 11 12 13
+
+
+
+
+
+ */
+
+
+namespace task3_solution
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
